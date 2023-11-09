08991ef64ded690abe623e3e3f31d271d8df1020
likalolika <pir0gee@yandex.ru>
2023-11-09 09:27:38 +0400

    js functions are posed in modeles

--- src/index.js
+++ src/index.js
@@ -17,79 +17,6 @@ import {
 } from "./components/modal.js";
 
 import { initialCards } from "./components/cards.js";
-/*const elbrusImage = new URL(
-  "./images/kirill-pershin-1404681-unsplash.png",
-  import.meta.url
-);
-const cherkesiaImage = new URL(
-  "./images/kirill-pershin-1088404-unsplash.jpg",
-  import.meta.url
-);
-const dombaiImage = new URL(
-  "./images/kirill-pershin-1556355-unsplash.png",
-  import.meta.url
-);
-
-const initialCards = [
-  {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-
-  {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
-
-  {
-    name: "Гора Эльбруc",
-    link: elbrusImage,
-  },
-
-  {
-    name: "Домбай",
-    link: dombaiImage,
-  },
-
-  {
-    name: "Гора Эльбрус",
-    link: elbrusImage,
-  },
-
-  {
-    name: "Карачаево-Черкессия",
-    link: cherkesiaImage,
-  },
-
-  {
-    name: "Домбай",
-    link: dombaiImage,
-  },
-];*/
-
-/*const cardsContainer = document.querySelector(".grid-block");
- const cardsTemplate = document.querySelector("#card-template").content;
- const templateElem = cardsTemplate.querySelector(".card-template__elem");*/
 
 function renderCard() {
   initialCards.forEach((item) => {
@@ -104,7 +31,6 @@ renderCard();
 
 const popupProfile = document.querySelector(".popup_profile-edit");
 const popupAddCard = document.querySelector(".popup_add-card");
-/*const popupShowImage = document.querySelector(".popup_show-image");*/
 const addButton = document.querySelector(".profile__add-button");
 const editButton = document.querySelector(".profile__edit-button");
 const closeBtnProfile = popupProfile.querySelector(".popup__close-button");
@@ -115,26 +41,9 @@ const closeBtnImage = popupShowImage.querySelector(
   ".popup__close-button_show-image"
 );
 
-/*function closeModalOver(evt) {
-  if (evt.target.classList.contains("popup")) {
-    closeModal(evt.target);
-  }
-}
-
-function closeOnOverlay(popup) {
-  popup.addEventListener("click", closeModalOver);
-}*/
-
 closeOnOverlay(popupProfile);
 closeOnOverlay(popupAddCard);
 closeOnOverlay(popupShowImage);
-
-/*export function handleEscClose (evt) {
-  if (evt.key === "Escape") {
-    const modalIsOpen = document.querySelector(".popup_opened");
-    modalIsOpen.classList.remove("popup_opened");
-  }
-}*/
 
 editButton.addEventListener("click", () => openModal(popupProfile));
 closeBtnProfile.addEventListener("click", () => closeModal(popupProfile));
@@ -194,18 +103,6 @@ function addCardButton(evt) {
 }
 
 formElem.addEventListener("submit", (evt) => addCardButton(evt));
-
-/*показать картинку*/
-
-/*const img = document.querySelector(".popup__image");
-const imgTitle = document.querySelector(".popup__description");
-
-function showImg(evt) {
-  openModal(popupShowImage);
-
-  img.setAttribute("src", evt.target.src);
-  imgTitle.textContent = evt.target.alt;
-}*/
 
 /*Закрыть  картинку*/
 
